bbcfd13b743add3e2f67fc47b20d665e4f3794b6
songhaetnim <minshinzz@naver.com>
2024-04-03 17:33:12 +0900

    React Native

--- src/todo_bs_components/TodoList.jsx
+++ src/todo_bs_components/TodoList.jsx
@@ -42,3 +42,45 @@ function getFilteredTodos(todos, filter) {
     return todos;
   return todos.filter(todo => todo.status === filter);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import { View, Image, StyleSheet } from 'react-native';
+
+// export default function CherryBlossom() {
+//   return (
+//     <View style={styles.container}>
+//       {/* 벚꽃 이미지 */}
+//       <Image
+//         source={require('./assets/cherry_blossom.png')}
+//         style={styles.cherryBlossom}
+//       />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#fff', // 배경색
+//   },
+//   cherryBlossom: {
+//     width: 200, // 이미지 너비
+//     height: 200, // 이미지 높이
+//     resizeMode: 'contain', // 이미지 크기 조정 방법
+//   },
+// });
