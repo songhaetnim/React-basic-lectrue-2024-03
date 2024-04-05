a54485d4ed66f047e84aa2d22a7195a0acd01844
songhaetnim <minshinzz@naver.com>
2024-04-05 14:05:45 +0900

    ReactMUI

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './apps/App21_router';
+// import App from './apps/App99_exam';
 import reportWebVitals from './reportWebVitals';
+import BasicModal from './mui/BasicModal';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div style={{marginTop:'40px', marginLeft: '20px', marginRight: '20px', }}>
+      <BasicModal />
+    </div>
   </React.StrictMode>
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
